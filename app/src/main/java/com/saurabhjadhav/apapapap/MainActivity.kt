6c992240256651f7b717e870c6403981123bb132
saurabhthesuperhero <saurabhthesuperhero@gmail.com>
2022-06-16 16:47:36 +0530

    init

---
+++ app/src/main/java/com/saurabhjadhav/apapapap/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.saurabhjadhav.apapapap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //write data binding for activity
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
