621efc0285f0396dba84c65b74da6ae3332eea55
saurabhthesuperhero <saurabhthesuperhero@gmail.com>
2022-06-18 23:49:54 +0530

    Added refresh layout

--- app/src/main/java/com/saurabhjadhav/apapapap/MainActivity.kt
+++ app/src/main/java/com/saurabhjadhav/apapapap/MainActivity.kt
@@ -2,10 +2,12 @@ package com.saurabhjadhav.apapapap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.saurabhjadhav.apapapap.adapter.RecyclerviewAdapter
 import com.saurabhjadhav.apapapap.databinding.ActivityMainBinding
 
@@ -17,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
         initRecyclerView()
         initViewModel()
+        iniRefreshListener()
     }
 
 
@@ -25,6 +28,20 @@ class MainActivity : AppCompatActivity() {
         recyclerviewAdapter = RecyclerviewAdapter()
         binding.recyclerview.adapter = recyclerviewAdapter
 
+    }
+
+
+    fun iniRefreshListener() {
+        val swipeRefreshLayout=findViewById<SwipeRefreshLayout>(R.id.swipe_layout)
+        swipeRefreshLayout.setOnRefreshListener { // This method gets called when user pull for refresh,
+            initViewModel()
+            val handler = Handler()
+            handler.postDelayed({
+                if (swipeRefreshLayout.isRefreshing()) {
+                    swipeRefreshLayout.setRefreshing(false)
+                }
+            }, 3000)
+        }
     }
     private fun initViewModel() {
         val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
